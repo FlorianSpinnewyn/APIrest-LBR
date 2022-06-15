c2fca9f6d68443a8aaaf80474d3d7a51947de284
Lumenir <56834525+EVanwormhoudt@users.noreply.github.com>
2022-06-15 10:15:30 +0200

    removed something

--- src/API/function_files.php
+++ src/API/function_files.php
@@ -468,11 +468,11 @@ function addFile( $request,$response,  $args) {
     }
 
     if($_FILES["file"]["type"] == "video/mpeg" || $_FILES["file"]["type"] == "video/avi" || $_FILES["file"]["type"] == "video/quicktime"|| $_FILES["file"]["type"] == "video/mov" || $_FILES['file']['type'] == "video/mp4"){
-        $getID3 = new getID3;
+        /* $getID3 = new getID3;
 
         $ThisFileInfo = $getID3->analyze($_FILES['file']['tmp_name']);
         echo json_encode($ThisFileInfo);
-        $duree = floor($ThisFileInfo['playtime_seconds']);
+        $duree = floor($ThisFileInfo['playtime_seconds']);*/
     }
     $file = $_FILES['file'];
 
